9cffe6374f1a22440c5a474c3533db0974dd7491
Nick Sabalausky <Abscissa>
2015-03-17 14:21:32 -0400

    Update license text.

--- libInputVisitor.d
+++ libInputVisitor.d
@@ -1,22 +1,25 @@
 ﻿/++
 Copyright (C) 2012 Nick Sabalausky <http://semitwist.com/contact>
 
-This program is free software. It comes without any warranty, to the extent
-permitted by applicable law. You can redistribute it and/or modify it under
-the terms of the Do What The Fuck You Want To Public License, Version 2,
-as shown below. Details are at: http://sam.zoy.org/wtfpl/
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://www.wtfpl.net/ for more details.
 
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-                    Version 2, December 2004
+	DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+				Version 2, December 2004 
 
- Everyone is permitted to copy and distribute verbatim or modified
- copies of this license document, and changing it is allowed as long
- as the name is changed.
+Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
 
-            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+Everyone is permitted to copy and distribute verbatim or modified 
+copies of this license document, and changing it is allowed as long 
+as the name is changed. 
 
-  0. You just DO WHAT THE FUCK YOU WANT TO. 
+		DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+
+0. You just DO WHAT THE FUCK YOU WANT TO.
 +/
 
 /++
